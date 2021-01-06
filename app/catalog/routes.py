348c4f818d5a3ae76a6c4c2ad9bbba8b18501190
girish.matta <girish.mpm@gmail.com>
2021-01-06 20:04:32 +1100

    Added the display_books route

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,5 +1,10 @@
+from flask.templating import render_template
 from app.catalog import main
+from app import db
+from app.catalog.models import Book, Publication
+
 
 @main.route('/')
-def hello():
-    return 'Hello World!'
+def display_books():
+    books = Book.query.all()
+    return render_template('home.html', books=books)
